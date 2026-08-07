9365c122c85b7bf6084635e2d6afc51938908c13
mariana-torres-v <mariana.torres@bykon.com.mx>
2026-08-06 23:23:31 -0600

    antes de empezar a automatizar la app

--- core/config.py
+++ core/config.py
@@ -25,6 +25,51 @@ class Config:
     # Screenshots
     SCREENSHOT_ON_FAILURE = True
 
+    # ===== Appium =====
+
+    APPIUM_SERVER = os.getenv(
+        "APPIUM_SERVER",
+        "http://127.0.0.1:4723"
+    )
+
+    PLATFORM_NAME = os.getenv(
+        "PLATFORM_NAME",
+        "Android"
+    )
+
+    AUTOMATION_NAME = os.getenv(
+        "AUTOMATION_NAME",
+        "UiAutomator2"
+    )
+
+    DEVICE_NAME = os.getenv(
+        "DEVICE_NAME",
+        "Android"
+    )
+
+    APP_PACKAGE = os.getenv(
+        "APP_PACKAGE",
+        "net.veritran.mvmx.p3.qa"
+    )
+
+    APP_ACTIVITY = os.getenv(
+        "APP_ACTIVITY",
+        "net.veritran.mvmx.p3.qa.VTCommonActivity"
+    )
+
+    NO_RESET = os.getenv(
+        "NO_RESET",
+        "True"
+    ).lower() == "true"
+
+    FORCE_APP_LAUNCH = os.getenv(
+        "FORCE_APP_LAUNCH",
+        "True"
+    ).lower() == "true"
+
+    #CLOSE_APP_ON_EXIT = True
+
+
     # ===== BrowserStack =====
     BROWSERSTACK_USERNAME = os.getenv("BROWSERSTACK_USERNAME", "")
     BROWSERSTACK_ACCESS_KEY = os.getenv("BROWSERSTACK_ACCESS_KEY", "")
